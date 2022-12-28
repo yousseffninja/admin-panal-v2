59f6cfe1cd9ab788c70dc3f5f39be3e2d2d955eb
Youssef Mohamed <81262678+yousseffninja@users.noreply.github.com>
2022-12-28 16:01:43 +0200

    Error handle

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-process.on('uncaughtException', (err) => {
-    console.log(err.name, err.message);
-    server.close(() => {
-        process.exit(1);
-    });
-});
+// process.on('uncaughtException', (err) => {
+//     console.log(err.name, err.message);
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
 
 dotenv.config({
     path: './config.env',
